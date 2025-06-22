47ff052a17fc5c65b33f8b2ffbadce38d5e4eaf4
xenozite <xeno.zite3@gmail.com>
2025-06-22 14:59:05 +0700

    Format the model's output into a JSON object

--- backend/chatbot/response_generator.py
+++ backend/chatbot/response_generator.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import json
 import os
 
 # Create your views here.
@@ -7,35 +8,56 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 def get_response(words):
     # System prompt
     system_prompt = f"""
-    Read the following content and:
-    1. Extract the main factual claim.
-    2. Determine whether it is likely a hoax or true.
-    3. Explain your reasoning.
+    Read the following content and return a structured JSON object with the following fields:
+    1. "main_claim" : Extract the main factual claim made in the text.
+    2. "verdict": Ranging from "True", "Likely True", "Unclear", or "Misinformation".
+    3. "reasoning": Explain your reasoning of how you arrived at the verdict.
+    4. "rating": an integer between 0 and 100 indicating how likely the claim is fake (0 = real, 100 = hoax)
 
-    Content:
-    \"\"\"{words}\"\"\"
+    Return only the JSON object. Do not include any explanation or formatting outside the JSON. 
+    """
+
+    user_prompt = f"""
+        Content:
+        \"\"\"{words}\"\"\"
     """
 
     # Make the api call
     response = client.chat.completions.create(
         model="gpt-4o-mini-2024-07-18",
         messages=[{
-            "role": "user",
+            "role": "system",
             "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": user_prompt
         }],
         temperature=0.8,
-        max_tokens=800
+        max_tokens=1000
     )
 
     return response
 
 def format_response(response):
     # Extract the claim and reasoning
-    claim = response.choices[0].message.content
+    content = response.choices[0].message.content
     # Remove unnecessary text or formatting
-    claim = claim.strip()
+    content = content.strip()
     
-    return claim
+    try:
+        # Parse json object into python dictionary
+        parsed = json.loads(content)
+        return parsed
+    except json.JSONDecodeError:
+        print("No valid JSON detected:")
+        print(content)
+        return{
+            "main_claim": "",
+            "verdict": "Unclear",
+            "reasoning": content,  # fallback to raw text
+            "rating": 50
+        }
 
 def generate_response(words):
     #call the OpenAI API
