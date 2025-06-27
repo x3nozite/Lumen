3b60ed7bd1fc0eba793129f3f9dce475eb63409f
farrelpit <farrelputra141@gmail.com>
2025-06-27 19:22:51 +0700

    Add user table and initial migration

--- backend/userauth/models.py
+++ backend/userauth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50, unique=True)
+    created = models.DateField(auto_now_add=True)
