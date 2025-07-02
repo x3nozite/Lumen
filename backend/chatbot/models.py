1594b3d359d27581bd1e5f8974637858ac6058c8
xenozite <xeno.zite3@gmail.com>
2025-07-02 21:52:50 +0700

    Create LumenResponse model (for database), and migrations

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class LumenResponse(models.Model):
+    main_claim = models.CharField(max_length=255)
+    verdict = models.CharField(max_length=31)
+    reasoning = models.TextField()
+    rating = models.IntegerField()
+    web_links = models.JSONField()
+    date = models.DateTimeField(auto_now_add=True)
