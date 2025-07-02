afddb2e48a04775464ea930eb46d9193b91bee3c
xenozite <xeno.zite3@gmail.com>
2025-07-02 20:07:36 +0700

    Change database to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME' : 'lumen_response',
+        'USER' : 'postgres',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
