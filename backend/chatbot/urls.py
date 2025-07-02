cfe9a4e67709ad26908a12588306a00576d81fde
xenozite <xeno.zite3@gmail.com>
2025-07-02 21:52:31 +0700

    Change urls to use gemini_response

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import chatbot_response
+from .views import chatbot_response, gemini_response
 
 urlpatterns = [
-    path("response/", chatbot_response, name="chatbot"),
+    path("response/", gemini_response, name="chatbot"),
 ]
