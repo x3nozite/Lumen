5a4d44ec5c531cff5eeec340ed3a8f32693b7312
xenozite <xeno.zite3@gmail.com>
2025-07-03 20:28:59 +0700

    Input now calls the POST request and shows the data in result screen

--- frontend/lib/screens/scan/result/result.dart
+++ frontend/lib/screens/scan/result/result.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
-  const ResultPage({super.key});
+  const ResultPage(this.data, {super.key});
+
+  final Map<String, dynamic> data;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Result Page')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Result Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Main claim: ${data['main_claim']}"),
+            Text("Reasoning: ${data['reasoning']}"),
+          ],
+        ),
+      ),
+    );
   }
 }
